3ba99caa9130befb5ea059a5568a76dc9c915989
shenguancong <shenguancong_v@didiglobal.com>
2022-08-17 18:15:42 +0800

    🚀 优化update

--- src/update.ts
+++ src/update.ts
@@ -1,35 +1,37 @@
 import path from 'path';
 import ora from 'ora';
 import shell from 'shelljs';
-import fs from 'fs-extra'
+import fs from 'fs-extra';
 
 export const updateVue2Components = () => {
   const loading = ora('download template');
   return new Promise((resolve, reject) => {
     try {
       loading.start('start update components');
-      shell.cd(path.join(__dirname, '../lib'))
+      shell.cd(path.join(__dirname, '../components-lib'))
       shell.exec('rm -rf .git')
       const updateFiles = () => {
-        if (fs.existsSync(path.join(__dirname, '../lib'))) {
-          shell.cd(path.join(__dirname, '../lib'))
+        if (fs.existsSync(path.join(__dirname, '../components-lib'))) {
+          shell.cd(path.join(__dirname, '../components-lib'))
           shell.rm('-rf', '*')
         }
-        shell.cd(__dirname)
+        shell.cd(path.join(__dirname, '../components-lib'))
         shell.exec('git pull origin master')
         shell.exec('rm -rf .git')
       }
 
       shell.exec('git init')
-      shell.exec('git remote add origin git@git.xiaojukeji.com:didibike/vue2-components.git') // 物料仓库
+      shell.exec('git remote add origin git@github.com:youuss/components-lib.git') // 物料仓库
       shell.exec('git config core.sparseCheckout true')
-      shell.exec("echo 'lib' >> .git/info/sparse-checkout")
+      shell.exec("echo 'vue2-lib/lib' >> .git/info/sparse-checkout")
       updateFiles()
+      shell.cd(path.join(__dirname, '../components-lib/vue2-lib/lib'))
+      shell.exec('rm -rf index.js')
       loading.stop();
     } catch (e) {
       reject(e)
       loading.stop();
-      loading.fail('download fail');
+      loading.fail('update fail');
     }
   })
 }
